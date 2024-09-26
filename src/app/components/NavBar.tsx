067d9b3630599bd89ec41e9d0e5af0edef9fce4c
jbgcap <jbgcap@gmail.com>
2024-09-26 13:09:54 +0900

    Link component use

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,31 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
 
 const NavBar = () => (
-  <header>
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand">Bookmarker</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link">Add Bookmark</a>
-            </li>
-          </ul>
+    <header>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <Link href="/" className="navbar-brand">
+              Bookmarker
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link href="/add" className="nav-link">
+                  Add Bookmark
+                </Link>            
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
-);
-
-export default NavBar;
+      </nav>
+    </header>
+  );
+  
+  export default NavBar;
+  
