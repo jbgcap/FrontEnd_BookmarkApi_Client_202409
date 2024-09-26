45d0b6b15261a9323372c57d269ef639eab8837a
jbgcap <jbgcap@gmail.com>
2024-09-26 15:40:09 +0900

    SearchForm add

--- src/lib/bookmarkApi.tsx
+++ src/lib/bookmarkApi.tsx
@@ -3,12 +3,20 @@ import axios from "axios"
 
 const API_BASE_URL = 'http://localhost:8080'
 
-export async function getAllBookmarks(page: number): Promise<BookmarksResponse> {
+export async function getAllBookmarks(page: number, query: string): Promise<BookmarksResponse> {
     
-    const API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
+    let API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
+    if(query) {
+        API_URL += `&query=${query}`
+    }
     console.log(`url = ${API_URL}`)
     
     const res = await axios.get<BookmarksResponse>(API_URL)
 
+    return res.data
+}
+
+export async function saveBookmark(bookmark:{title: string, url: string}) {
+    const res = await axios.post(`${API_BASE_URL}/api/bookmarks`, bookmark)
     return res.data
 }
