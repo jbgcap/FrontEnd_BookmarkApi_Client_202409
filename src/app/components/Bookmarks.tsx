bf1dc386429045e817519f3e7801affc8bc2b6e4
jbgcap <jbgcap@gmail.com>
2024-09-26 14:23:50 +0900

    Bookmark / Bookmarks component add

---
+++ src/app/components/Bookmarks.tsx
@@ -0,0 +1,20 @@
+import { BookmarkType } from '@/types';
+import React from 'react';
+import Bookmark from './Bookmark';
+
+type Props = {
+    bookmarks: BookmarkType[]
+}
+
+const Bookmarks: React.FC<Props> = ({ bookmarks }) => {
+    return (
+        <div>
+            <h1>Welcome to Bookmarker</h1>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+            }
+        </div>
+    );
+};
+
+export default Bookmarks;
