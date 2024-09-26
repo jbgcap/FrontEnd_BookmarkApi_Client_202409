8a239a4ec4dac8e2869b560443bcd1433b3fefcd
jbgcap <jbgcap@gmail.com>
2024-09-26 11:45:55 +0900

    layout.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import NavBar from "@/app/components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <main>
-        <h1>Welcome to Bookmarker</h1>
-      </main>
+      <h1>Welcome to Bookmarker</h1>
     </div>
   );
 }
